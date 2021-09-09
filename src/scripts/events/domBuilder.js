e7be9cc999032490f6f3654ff3c4de5fb69c18c1
Austin Parker <austincparker@gmail.com>
2021-09-08 19:30:24 -0500

    added vocab form and connected the create entry button to it

--- src/scripts/events/domBuilder.js
+++ src/scripts/events/domBuilder.js
@@ -2,6 +2,7 @@ const domBuilder = () => {
   document.querySelector('#app').innerHTML = `
     <div id="navbar"></div>
     <div id="main-container">
+        <div id="form-container"></div>
         <div id="languages"></div>
         <div id="vocab">
         </div>
